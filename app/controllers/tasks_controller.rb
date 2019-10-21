b5968a81932c9bb2735f310a1421f06365253f99
Shizue Chalwell <shizue46@gmail.com>
2019-10-21 07:38:25 +0000

    Tasks onto Page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
     def index
+        #loads tasks from database
+        #render json:  show the user the json representation of the tasks
         render json: Task.all
     end
 end
